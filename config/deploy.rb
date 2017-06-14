2a599b8be4b07d7e8e05238bd2c9b83db316a0d2
cebenavides <cebenavides@uninorte.edu.co>
2017-06-14 15:59:04 +0000

    Added support for Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+# config valid only for current version of Capistrano
+lock "3.8.1"
+
+set :application, "groups-vip-un"
+set :repo_url, "git@github.com:cebenavides/groups-vip-un.git"
+
+set :deploy_to, '/home/ubuntu/groups-vip-un'
+
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
